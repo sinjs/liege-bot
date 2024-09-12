cfb2302c07add08c6b76a6b78bb9efaea7dece09
sinjs <50749166+sinjs@users.noreply.github.com>
2024-09-12 18:04:32 +0200

    Refactor AI command

--- src/commands/ai.ts
+++ src/commands/ai.ts
@@ -2,8 +2,9 @@ import {
   ApplicationCommandOptionType,
   InteractionResponseType,
   MessageFlagsBitField,
+  type APIApplicationCommandInteractionDataSubcommandOption,
 } from "discord.js";
-import type { Command } from "../commands";
+import type { APICommand, Command } from "../commands";
 import {
   editInitialResponse,
   embeds,
@@ -18,6 +19,186 @@ import { db } from "../lib/db";
 import ky from "ky";
 import { insertUserAction } from "../lib/user-actions";
 import { UserActionType } from "@prisma/client";
+
+type APISubcommand =
+  Required<APIApplicationCommandInteractionDataSubcommandOption>;
+
+const tokenNotLinkedResponse = {
+  type: InteractionResponseType.ChannelMessageWithSource,
+  data: {
+    embeds: embeds({ type: "error" }, (embed) =>
+      embed.setDescription(
+        `❌ You do not have an AI token linked. Use \`/ai link\` to link your token.`
+      )
+    ),
+  },
+};
+
+async function getToken(userID: string) {
+  const linkedToken = await db.linkedAIToken.findFirst({
+    where: { user_id: userID },
+  });
+
+  return linkedToken;
+}
+
+async function generateImage(command: APICommand, subcommand: APISubcommand) {
+  const user = getUser(command);
+
+  const imagePromptOption = getSubcommandOption<StringOption>(
+    subcommand.options,
+    "prompt"
+  );
+
+  const token = await getToken(user.id);
+
+  if (!token) return tokenNotLinkedResponse;
+
+  async function run() {
+    const res = await ky.post("https://ai.nigga.church/v2/generate/image", {
+      headers: { Authorization: token!.ai_token },
+      json: {
+        prompt: imagePromptOption.value,
+      },
+      timeout: 30 * 1000,
+    });
+
+    const { imageURL } = await res.json<{ imageURL: string }>();
+
+    await insertUserAction({
+      user_id: user.id,
+      type: UserActionType.AIGenerateImage,
+      ai_prompt: imagePromptOption.value,
+      ai_result: imageURL,
+    });
+
+    await editInitialResponse(command.token, {
+      content: imageURL,
+    });
+  }
+
+  return new Promise((resolve) => {
+    resolve({
+      type: InteractionResponseType.DeferredChannelMessageWithSource,
+    });
+    run();
+  });
+}
+
+async function generateText(command: APICommand, subcommand: APISubcommand) {
+  const user = getUser(command);
+
+  const textPromptOption = getSubcommandOption<StringOption>(
+    subcommand.options,
+    "prompt"
+  );
+
+  const token = await getToken(user.id);
+
+  if (!token) return tokenNotLinkedResponse;
+
+  async function run() {
+    const res = await ky.post("https://ai.nigga.church/v2/generate/text", {
+      headers: { Authorization: token!.ai_token },
+      json: {
+        model: "llama-3-8b-instruct",
+        messages: [
+          {
+            role: "user",
+            content: textPromptOption.value,
+          },
+        ],
+      },
+      timeout: 30 * 1000,
+    });
+
+    const { response } = await res.json<{ response: string }>();
+
+    await insertUserAction({
+      user_id: user.id,
+      type: UserActionType.AIGenerateText,
+      ai_prompt: textPromptOption.value,
+      ai_result: response,
+    });
+
+    await editInitialResponse(command.token, {
+      content: response,
+    });
+  }
+
+  return new Promise((resolve) => {
+    resolve({
+      type: InteractionResponseType.DeferredChannelMessageWithSource,
+    });
+    run();
+  });
+}
+
+async function linkToken(command: APICommand, subcommand: APISubcommand) {
+  const user = getUser(command);
+
+  const tokenOption = getSubcommandOptionalOption<StringOption>(
+    subcommand.options,
+    "token"
+  );
+
+  if (tokenOption) {
+    const response = await ky.get("https://ai.nigga.church/currentKey", {
+      headers: { Authorization: tokenOption.value },
+      throwHttpErrors: false,
+    });
+
+    if (!response.ok)
+      return {
+        type: InteractionResponseType.ChannelMessageWithSource,
+        data: {
+          flags: MessageFlagsBitField.Flags.Ephemeral,
+          embeds: embeds({ type: "error" }, (embed) =>
+            embed.setDescription(`❌ The provided AI token is not valid.`)
+          ),
+        },
+      };
+
+    await db.linkedAIToken.upsert({
+      where: { user_id: user.id },
+      create: { user_id: user.id, ai_token: tokenOption.value },
+      update: { ai_token: tokenOption.value },
+    });
+  } else {
+    const token = await getToken(user.id);
+
+    if (!token)
+      return {
+        type: InteractionResponseType.ChannelMessageWithSource,
+        data: {
+          flags: MessageFlagsBitField.Flags.Ephemeral,
+          embeds: embeds({ type: "success" }, (embed) =>
+            embed.setDescription(
+              `❌ You need to have a token linked to remove it. Use \`/ai link <token>\` to link a token.`
+            )
+          ),
+        },
+      };
+
+    await db.linkedAIToken.delete({
+      where: { user_id: user.id },
+    });
+  }
+
+  return {
+    type: InteractionResponseType.ChannelMessageWithSource,
+    data: {
+      flags: MessageFlagsBitField.Flags.Ephemeral,
+      embeds: embeds({ type: "success" }, (embed) =>
+        embed.setDescription(
+          `✅ Successfully ${
+            tokenOption ? "updated" : "removed"
+          } the AI token ${tokenOption ? "in" : "from"} your account.`
+        )
+      ),
+    },
+  };
+}
 
 export default {
   data: new ExtendedSlashCommandBuilder()
@@ -64,172 +245,12 @@ export default {
     if (!subcommand || !subcommand.options)
       throw new OptionNotSpecifiedError("subcommand");
 
-    const user = getUser(command);
-
     switch (subcommand.name) {
       case "text":
-        async function runTextCommand() {
-          const res = await ky.post(
-            "https://ai.nigga.church/v2/generate/text",
-            {
-              headers: { Authorization: linkedToken!.ai_token },
-              json: {
-                model: "llama-3-8b-instruct",
-                messages: [
-                  {
-                    role: "user",
-                    content: textPromptOption.value,
-                  },
-                ],
-              },
-              timeout: 30 * 1000,
-            }
-          );
-
-          const { response } = await res.json<{ response: string }>();
-
-          await insertUserAction({
-            user_id: user.id,
-            type: UserActionType.AIGenerateText,
-            ai_prompt: textPromptOption.value,
-            ai_result: response,
-          });
-
-          await editInitialResponse(command.token, {
-            content: response,
-          });
-        }
-
-        const textPromptOption = getSubcommandOption<StringOption>(
-          subcommand.options,
-          "prompt"
-        );
-
-        const linkedToken = await db.linkedAIToken.findFirst({
-          where: { user_id: user.id },
-        });
-
-        if (!linkedToken)
-          return {
-            type: InteractionResponseType.ChannelMessageWithSource,
-            data: {
-              embeds: embeds({ type: "error" }, (embed) =>
-                embed.setDescription(
-                  `❌ You do not have an AI token linked. Use \`/ai link\` to link your token.`
-                )
-              ),
-            },
-          };
-
-        return new Promise((resolve) => {
-          resolve({
-            type: InteractionResponseType.DeferredChannelMessageWithSource,
-          });
-          runTextCommand();
-        });
 
       case "image":
-        async function runImageCommand() {
-          const res = await ky.post(
-            "https://ai.nigga.church/v2/generate/image",
-            {
-              headers: { Authorization: linkedImageToken!.ai_token },
-              json: {
-                prompt: imagePromptOption.value,
-              },
-              timeout: 30 * 1000,
-            }
-          );
-
-          const { imageURL } = await res.json<{ imageURL: string }>();
-
-          await insertUserAction({
-            user_id: user.id,
-            type: UserActionType.AIGenerateImage,
-            ai_prompt: imagePromptOption.value,
-            ai_result: imageURL,
-          });
-
-          await editInitialResponse(command.token, {
-            content: imageURL,
-          });
-        }
-
-        const imagePromptOption = getSubcommandOption<StringOption>(
-          subcommand.options,
-          "prompt"
-        );
-
-        const linkedImageToken = await db.linkedAIToken.findFirst({
-          where: { user_id: user.id },
-        });
-
-        if (!linkedImageToken)
-          return {
-            type: InteractionResponseType.ChannelMessageWithSource,
-            data: {
-              embeds: embeds({ type: "error" }, (embed) =>
-                embed.setDescription(
-                  `❌ You do not have an AI token linked. Use \`/ai link\` to link your token.`
-                )
-              ),
-            },
-          };
-
-        return new Promise((resolve) => {
-          resolve({
-            type: InteractionResponseType.DeferredChannelMessageWithSource,
-          });
-          runImageCommand();
-        });
 
       case "link":
-        const tokenOption = getSubcommandOptionalOption<StringOption>(
-          subcommand.options,
-          "token"
-        );
-
-        if (tokenOption) {
-          const response = await ky.get("https://ai.nigga.church/currentKey", {
-            headers: { Authorization: tokenOption.value },
-            throwHttpErrors: false,
-          });
-
-          if (!response.ok)
-            return {
-              type: InteractionResponseType.ChannelMessageWithSource,
-              data: {
-                flags: MessageFlagsBitField.Flags.Ephemeral,
-                embeds: embeds({ type: "error" }, (embed) =>
-                  embed.setDescription(`❌ The provided AI token is not valid.`)
-                ),
-              },
-            };
-
-          await db.linkedAIToken.upsert({
-            where: { user_id: user.id },
-            create: { user_id: user.id, ai_token: tokenOption.value },
-            update: { ai_token: tokenOption.value },
-          });
-        } else {
-          await db.linkedAIToken.delete({
-            where: { user_id: user.id },
-          });
-        }
-
-        return {
-          type: InteractionResponseType.ChannelMessageWithSource,
-          data: {
-            flags: MessageFlagsBitField.Flags.Ephemeral,
-            embeds: embeds({ type: "success" }, (embed) =>
-              embed.setDescription(
-                `✅ Successfully ${
-                  tokenOption ? "updated" : "removed"
-                } the AI token ${tokenOption ? "in" : "from"} your account.`
-              )
-            ),
-          },
-        };
     }
 
     throw new Error("Subcommand ${subcommand.name} does not exist");
