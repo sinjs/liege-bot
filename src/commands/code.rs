4ab51c30a89e32f1e4b9e34dbfe34b8cbb4ea8ae
sin <50749166+sinjs@users.noreply.github.com>
2025-01-02 17:46:44 +0100

    feat(commands/code): command registration

--- src/commands/code.rs
+++ src/commands/code.rs
@@ -1,6 +1,9 @@
 use std::sync::Arc;
 
-use serenity::all::{CommandInteraction, CreateCommand};
+use serenity::all::{
+    CommandInteraction, CommandOptionType, CreateCommand, CreateCommandOption, InstallationContext,
+    InteractionContext,
+};
 
 use crate::{error::Error, AppState};
 
@@ -9,9 +12,33 @@ use super::Command;
 pub struct CodeCommand;
 
 impl Command for CodeCommand {
-    async fn run(interaction: CommandInteraction, state: Arc<AppState>) -> Result<(), Error> {}
+    async fn run(interaction: CommandInteraction, state: Arc<AppState>) -> Result<(), Error> {
+        Ok(())
+    }
 
     fn register() -> CreateCommand {
         CreateCommand::new("code")
+            .description("Execute code")
+            .integration_types(vec![InstallationContext::Guild, InstallationContext::User])
+            .contexts(vec![
+                InteractionContext::Guild,
+                InteractionContext::BotDm,
+                InteractionContext::PrivateChannel,
+            ])
+            .add_option(
+                CreateCommandOption::new(
+                    CommandOptionType::String,
+                    "language",
+                    "Programming language to use",
+                )
+                .required(true)
+                .add_string_choice("JavaScript", "js")
+                .add_string_choice("Rust", "rust")
+                .add_string_choice("Python", "python"),
+            )
+            .add_option(
+                CreateCommandOption::new(CommandOptionType::String, "code", "The code to execute")
+                    .required(false),
+            )
     }
 }
