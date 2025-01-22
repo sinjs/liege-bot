fcb4cad41330b97d41c00eeb7aadd6d478fee395
sinjs <50749166+sinjs@users.noreply.github.com>
2025-01-22 14:23:13 +0100

    feat: refactor sidebar

--- apps/frontend/src/components/app-sidebar.tsx
+++ apps/frontend/src/components/app-sidebar.tsx
@@ -20,64 +20,59 @@ import {
 import { ModeToggle } from "./mode-toggle";
 import { Link } from "react-router";
 
-const items = {
-  ai: [
-    {
-      title: "Chat",
-      url: "/ai/chat",
-      icon: MessageCircleIcon,
-    },
-    {
-      title: "Image",
-      url: "/ai/image",
-      icon: ImageIcon,
-    },
-  ],
-  execution: [
-    { title: "Code", url: "/execution/code", icon: CodeIcon },
-    { title: "Math", url: "/execution/math", icon: SigmaIcon },
-  ],
-};
+const groups = [
+  {
+    title: "AI",
+    items: [
+      {
+        title: "Chat",
+        url: "/ai/chat",
+        icon: MessageCircleIcon,
+      },
+      {
+        title: "Image",
+        url: "/ai/image",
+        icon: ImageIcon,
+      },
+    ],
+  },
+  {
+    title: "Execution",
+    items: [
+      { title: "Code", url: "/execution/code", icon: CodeIcon },
+      {
+        title: "Math",
+        url: "/execution/math",
+        icon: SigmaIcon,
+      },
+    ],
+  },
+];
 
 export function AppSidebar() {
   return (
     <Sidebar>
       <SidebarHeader />
       <SidebarContent>
-        <SidebarGroup>
-          <SidebarGroupLabel>AI</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.ai.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <Link to={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
-        <SidebarGroup>
-          <SidebarGroupLabel>Execution</SidebarGroupLabel>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              {items.execution.map((item) => (
-                <SidebarMenuItem key={item.title}>
-                  <SidebarMenuButton asChild>
-                    <Link to={item.url}>
-                      <item.icon />
-                      <span>{item.title}</span>
-                    </Link>
-                  </SidebarMenuButton>
-                </SidebarMenuItem>
-              ))}
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+        {groups.map((group) => (
+          <SidebarGroup key={group.title}>
+            <SidebarGroupLabel>{group.title}</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {group.items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <Link to={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </Link>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        ))}
       </SidebarContent>
       <SidebarFooter>
         <ModeToggle />
