67318d3fd08de3cfd113b22cfa5bf31087501091
sinjs <50749166+sinjs@users.noreply.github.com>
2025-06-19 23:28:13 +0200

    feat: change model to perplexity sonar pro

--- apps/backend/src/handlers/commands/ai.rs
+++ apps/backend/src/handlers/commands/ai.rs
@@ -109,7 +109,7 @@ impl AiCommand {
             .header("Authorization", &ENV.ai_token)
             .json(
                 &GenerateTextRequest::new()
-                    .model("perplexity-sonar")
+                    .model("perplexity-sonar-pro")
                     .add_message(GenerateTextMessage::new(GenerateTextMessageRole::System, "You are Liege, a friendly and helpful chatbot designed to assist users with various inquiries. Your responses should be:
 
 1. **Concise & Relevant** – Provide clear, direct answers without unnecessary elaboration.  
